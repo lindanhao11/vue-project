317f523d7c88250414272517ee71f8fc6a503efa
lin <3043638603@qq.com>
2024-03-02 23:40:23 +0800

    vue3的购物平台

---
+++ src/views/Home/components/HomePanel.vue
@@ -0,0 +1,63 @@
+<!-- 可复用组件的封装！！！！ -->
+
+<!-- 父组件中直接传不同数据进入panel，会有不用渲染结果 -->
+<!-- 需要用到这个组件的地方，传入不同的数据、插槽可变成不同内容 -->
+<script setup>
+// 定义props
+defineProps({
+  // 主标题
+  title: {
+    type: String
+  },
+  // 副标题
+  subTitle: {
+    type: String
+  }
+})
+
+</script> 
+
+
+<template>
+  <!-- 抽象成组件插槽 -->
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+        <!-- 主标题和副标题 -->
+        <h3>
+          {{ title }}<small>{{ subTitle }}</small>
+        </h3>
+      </div>
+      <!-- 主体内容区域 -->
+      <!-- 插槽 -->
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+.home-panel {
+  background-color: #fff;
+
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;
+
+    h3 {
+      flex: 1;
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
+
+// 触发获取导航列表的action
+
+import { useCategoryStore } from '@/stores/categoryStore'
+import { onMounted } from 'vue'
+
+//使用状态库
+const categoryStore = useCategoryStore()
+// 获取数据，数据使用是在layout的子组件中使用
+onMounted(() => categoryStore.getCategory())
+</script>
+
+<template>
+  <LayoutFixed />
+  <LayoutNav />
+  <LayoutHeader />
+  <!-- 添加key 破坏复用机制 强制销毁重建 -->
+  <!-- <RouterView :key="$route.fullPath" /> -->
+                                              <!-- 二级路由出口 默认加载home-->
+  <RouterView />
+  <LayoutFooter />
+</template>
